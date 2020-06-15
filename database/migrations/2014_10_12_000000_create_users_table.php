0d4037bcad3b4c47647f401ae1f294a5213459cc
Mahfujur51 <overcastmoon@gmail.com>
2020-06-15 09:43:39 +0600

    database migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('admin')->defualt('false');
+            $table->boolean('author')->defualt('false');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
