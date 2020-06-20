7a60f90dff5283d835ec3f3ea03b0a87c846f94f
Mahfujur51 <overcastmoon@gmail.com>
2020-06-20 14:34:50 +0600

    product controller created

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -36,6 +36,36 @@
             <hr>
             @endforeach
         </div>
+        @if (Auth::check())
+        <div class="row">
+            <div class="col-md-12">
+                    <form action="" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <label for="">Your Comment</label>
+                            <textarea name="content" class="form-control" id="" cols="30" rows="10"></textarea>
+                        </div>
+                         <input type="hidden" name="post_id" value="{{$post->id}}">
+
+                        <div class="from-group">
+                            <div class="text-center">
+                                <input type="submit" value="Comment Submit" class="btn btn-success">
+                            </div>
+                        </div>
+                    </form>
+            </div>
+        </div>
+        @else
+        <div class="row">
+            <div class="col-md-12">
+                <div class="text-center">
+                <a href="{{ route('login') }}" class="btn btn-success">Login</a> <br> <br>
+                <a href="{{ route('register') }}" class="btn btn-info">Register</a>
+
+            </div>
+            </div>
+        </div>
+        @endif
     </div>
 </article>
 <hr>
