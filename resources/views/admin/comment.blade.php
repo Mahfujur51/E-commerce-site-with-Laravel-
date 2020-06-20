8e48ea7b0fe1d18dbd3b9f9b91a2590f6f400c80
Mahfujur51 <overcastmoon@gmail.com>
2020-06-20 11:13:54 +0600

    delete return model

--- resources/views/admin/comment.blade.php
+++ resources/views/admin/comment.blade.php
@@ -30,7 +30,7 @@
                             <td>{{$comment->content}}</td>
                             <td>{{$comment->created_at->diffForHumans()}}</td>
                             <td>
-                                <a href="{{ route('admin.comment.delete',$comment->id) }}" class="btn btn-danger"> Delete</a>
+                                <button  data-toggle="modal" data-target="#exampleModal-{{$comment->id}}" class="btn btn-danger">Delete</button>
                             </td>
                         </tr>
                         @endforeach
@@ -46,4 +46,26 @@
         </div>
     </div>
 </div>
+<!-- Modal -->
+@foreach ($comments as $post)
+    {{-- expr --}}
+
+<div class="modal fade" id="exampleModal-{{$post->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">{{$post->content}}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <a href="{{ route('admin.comment.delete',$post->id) }}" type="button" class="btn btn-danger">Are you sure to delete</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endforeach
 @endsection
--- resources/views/admin/post.blade.php
+++ resources/views/admin/post.blade.php
@@ -19,33 +19,52 @@
                         </tr>
                     </thead>
                     <tbody>
-                     @if ($posts->count()>0)
-                         {{-- expr --}}
-
-                       @foreach ($posts as $key=>$post)
-
+                        @if ($posts->count()>0)
+                        {{-- expr --}}
+                        @foreach ($posts as $key=>$post)
                         <tr>
                             <td>{{$key+1}}</td>
                             <td >{{$post->title}}</td>
                             <td>{{$post->comments->count()}}</td>
-
                             <td>{{$post->created_at->diffForHumans()}}</td>
                             <td>
-                                <a href="{{ route('admin.post.delete',$post->id) }}" class="btn btn-danger"> Delete</a>
-                                 <a href="{{ route('admin.post.edit',$post->id) }}" class="btn btn-success">Edit</a>
+                                {{-- <a href="{{ route('admin.post.delete',$post->id) }}"  --}}
+                                    <button class="btn btn-danger"  data-toggle="modal" data-target="#exampleModal-{{$post->id}}"> Delete</button>
+                                <a href="{{ route('admin.post.edit',$post->id) }}" class="btn btn-success">Edit</a>
                             </td>
                         </tr>
                         @endforeach
                         @else
                         <tr >
-                          <td colspan="5" class="text-center">No Comment Available!!</td>
-                      </tr>
-                      @endif
-
-                  </tbody>
-              </table>
-          </div>
-      </div>
-  </div>
+                            <td colspan="5" class="text-center">No Comment Available!!</td>
+                        </tr>
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
+<!-- Modal -->
+@foreach ($posts as $post)
+    {{-- expr --}}
+
+<div class="modal fade" id="exampleModal-{{$post->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">{{$post->title}}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <a href="{{ route('admin.post.delete',$post->id) }}" type="button" class="btn btn-danger">Are you sure to delete</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endforeach
 @endsection
